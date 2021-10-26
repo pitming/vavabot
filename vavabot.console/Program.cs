e9b2c75af9435abda33fc1b20a019220c6a30926
pitming <pm.pitming@gmail.com>
2021-10-26 21:16:44 +0200

    first commit

---
+++ vavabot.console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace vavabot.console
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      using IWebDriver driver = new ChromeDriver();
+    }
+  }
+}
